5d6557f6f5321b9fa4e2eff20ff78675a1a861e6
Ramadhany <rpebspramesta@gmail.com>
2022-11-15 19:20:08 +0700

    upload sc

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.admin')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-3">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>Barang</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-shopping-cart"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>Surat</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-envelope-open-text"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-gradient-warning">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>Obat</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-capsules"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-gradient-primary">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>Admin</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-users"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
